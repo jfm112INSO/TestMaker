4f5ea4b75948c4917b39380c0a74d56cd4dd82d9
jfm112INSO <jfm112@inlumine.ual.es>
2025-06-18 01:30:07 +0200

    a

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,14 +10,10 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...compat.config({
-    extends: ['next'],
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
-  }),
+  {
+    ignores: ["**/*"],
+    rules: {},
+  },
 ];
 
 export default eslintConfig;
